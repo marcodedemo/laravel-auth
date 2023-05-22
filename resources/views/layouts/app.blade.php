37b6be44d9881ca8304628ea691c858b3fa035ba
marcodedemo <marco.dedemo@gmail.com>
2023-05-22 12:59:00 +0200

    feat: changed pages layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{url('/admin/projects') }}">{{ __('Home') }}</a>
+                            <a class="nav-link" href="{{url('/admin/projects') }}">{{ __('Projects') }}</a>
                         </li>
                     </ul>
 
